00305282962518b37427abae054f035ab45ee396
Kaito0523 <kaito.tanabe0523@gmail.com>
2024-09-05 17:04:55 +0900

    setting.py

--- assist/settings.py
+++ assist/settings.py
@@ -27,7 +27,6 @@ IMAGE_URL = '/images/'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tro%r=nvfio2+s#9+-cujetn#%xllmgdqe62bbc-ji@)#k-@t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
